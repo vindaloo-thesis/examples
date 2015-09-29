5d9d257d3477dde8eb650c2d74b9930b25b55056
Robert Edstrom <legogris@legogris.se>
2015-09-29 16:51:45 +0200

    namecoind example 1

--- Vindaloo.hs
+++ Vindaloo.hs
@@ -1,3 +1,4 @@
+module Vindaloo where
 import Data.Bool
 import Data.Int
 import qualified Data.ByteString as BS
@@ -5,15 +6,18 @@ import Data.Word
 import Data.DoubleWord
 import Data.ByteString
 
-newtype Address = Address String deriving (Show)
-newtype SHAHash = SHAHash String deriving (Show)
+newtype Address = Address ByteString deriving (Show)
+newtype SHAHash = SHAHash ByteString deriving (Show)
 
 class EVMVar a where
   serialize :: a -> EVMVarT
+
+class EVMVar a => Public a where
+  expose :: a -> EVMVarT
 
 data EVMVarT = ByteString ByteString
              | Bool Bool
              | Int8 Int8
              | UInt8 Word8 --[...]
              | Int256 Int256
-             | UInt256 Word256
+             | UInt256 Word256 deriving (Show, Read, Eq, Ord)
--- rps.hs
+++ rps.hs
@@ -1,4 +1,11 @@
+module Name (state, fun1, fun2) where
+
+state  = DB { players = 0 }
+
 import Vindaloo
+import Control.Monad.State.Lazy
+
+data Contract  = DB { state :: DB, functions :: []}
 
 data DB = DB { players :: (Player, Player)
              , num_players :: Int
@@ -15,9 +22,12 @@ data Player = Player { address :: Address
 
 data Choice = Rock | Paper | Scissors deriving (Show)
 
+init :: () -> State DB
+init = modify \db -> db { num_players = 0 }
 
-init :: () -> DB
-init = undefined
+player_input :: Choice -> State DB Int8
+player_input hash = do
+
 
 -- player_input :: SHAHash -> State DB RetVal
 -- player_input hash = do
