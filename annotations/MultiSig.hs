fa8ef953f53819319283179f686b444cf8fff8ec
weeeeeew <jackpettersson@gmail.com>
2015-10-07 14:42:17 +0200

    bugfix in MultiSig

--- annotations/MultiSig.hs
+++ annotations/MultiSig.hs
@@ -18,7 +18,7 @@ data Tx = Tx { to       :: Address
              , signedBy :: [Address]
              }
 
-init :: [Address] -> Int -> Ethereum TxState ()
+init (0) :: [Address] -> Int -> Ethereum TxState ()
 init addrs n | length addrs < n = fail "fewer signers than required signatures"
              | otherwise        = S.put $ TxState { transaction = Nothing
                                                   , signers     = addrs
@@ -36,10 +36,10 @@ For finalize, it shows the minimum number of participants required before the
 function can be called.
 -}
 send ((),1) :: Address -> Int -> Ethereum TxState Int
-send to amount = S.modify $ \s -> s{ transaction = Just (createTx to amount sender) }
+send to amount | sender `elem` signers state = S.modify $ \s -> s{ transaction = Just (createTx to amount sender) }
 
 sign ((),1) :: Ethereum TxState ()
-sign = S.modify $ \s -> s{ transaction = addSigner (transaction s) } -- we reeeeeally need better syntax for updating the state. make (a subset of) lenses part of the stdlib?
+sign | sender `elem` signers state = S.modify $ \s -> s{ transaction = addSigner (transaction s) } -- we reeeeeally need better syntax for updating the state. make (a subset of) lenses part of the stdlib?
   where
     addSigner Nothing   = Nothing
     addSigner (Just tx) = let signedBy' = L.insert a (signedBy tx)
