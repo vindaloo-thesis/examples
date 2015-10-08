bcc696856901df30247d9d4c2f6dfc8532b385ab
weeeeeew <jackpettersson@gmail.com>
2015-10-08 16:09:22 +0200

    lenses/RPS and /MultiSig

---
+++ lenses/rps.hs
@@ -0,0 +1,34 @@
+contract RPS
+( playerChoice
+, finalize ) where
+
+import Control.Lens ((%=),(+=),(^.))
+import Data.Map (empty,assocs)
+
+state = { players  = empty :: Map Address (Commit Choice)
+        , nPlayers = 0     :: Int
+        }
+
+playerChoice :: Commit Choice -> Ethereum ()
+playerChoice c | state^.nPlayers == 2 = fail "Game full"
+               | otherwise            = do players %= insert sender c
+                                           nPlayers += 1 -- (+=) is the increment operator for lenses
+
+finalize :: Ethereum ()
+finalize | state^.nPlayers /= 2 = fail "No game to finalize"
+         | otherwise            = do
+    c1 <- open cc1
+    c2 <- open cc2
+    case winner c1 c2 of
+      First  -> send (address p1) balance
+      Second -> send (address p2) balance
+      Tie    -> send (address p1) (balance/2) >> send (address p2) (balance/2)
+  where
+    (p1,cc1):(p2,cc2):_ = assocs (state^.players)
+
+winner :: Choice -> Choice -> Result
+winner c1 c2  | c1 == c2 = Tie
+winner Paper    Rock     = First
+winner Rock     Scissors = First
+winner Scissors Paper    = First
+winner _        _        = Second
