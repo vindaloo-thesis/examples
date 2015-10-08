2769437472792915ced6f672c2e5d967a983748d
weeeeeew <jackpettersson@gmail.com>
2015-10-08 12:54:17 +0200

    Started experimenting with explicit state and lenses

---
+++ lenses/NameCoin.hs
@@ -0,0 +1,41 @@
+contract NameCoin
+( register
+, get ) where
+
+import Control.Lens.Setter ((%=),(^.))
+import Data.Map (lookup)
+import Data.Maybe (fromMaybe)
+
+-- The state is declared as a special data structure. It is an implicit
+-- parameter to the Ethereum monad.
+state = { registry :: Map Word32 Word32 }
+
+{- Two assumptions are made below:
+
+1. 'state' is the current value of the state. (Regular value, not monadic!)
+   It has the type 'State'.
+2. A lens has automatically been generated (there exist functions for doing
+   that) for all fields in the state, with the same name as the field has in
+   the source code. Such a name clash is not allowed, but possible to sugar
+   away *if* we rename the fields themselves to something else, e.g. '_registry'.
+   
+   To be clear:
+   registry  :: Lens State (Map Word32 Word32)
+   _registry :: State -> Map Word32 Word32
+   
+   This might not be worth it.
+
+(^.) is an operator for accessing the value a lens is focused on from a
+data structure. In this case, it simply retrieves the value.
+
+(%=) is an operator for modifying the value a lens is focused on using a
+function, when it is in a stateful monad. In this case, the supplied function
+inserts the supplied value at the supplied key.
+-}
+register :: Word32 -> Word32 -> Ethereum ()
+register key val | key `member` state^.registry = fail "Key taken"
+                 | otherwise                    = registry %= insert key val
+
+-- Nothing weird here if you understood the previous function. :)
+get :: Word32 -> Ethereum Word32
+get key = return $ fromMaybe 0 $ lookup key (state^.registry)
